212888ead7a8208525bd7519bfb4a49c420c51ee
Red Wei <hevilhuy2@gmail.com>
2021-07-07 11:21:36 +0700

    Added initilize and dispose component example.

---
+++ BlazorComponent/Data/ComponentLifecycle/Car.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorComponent.Data.ComponentLifecycle
+{
+    public class Car
+    {
+        private string _color;
+
+        public string Color
+        {
+            get => _color;
+            set
+            {
+                _color = value;
+                OnColorChanged?.Invoke(this, value);
+            }
+        }
+
+        public event OnColorChangedHandler OnColorChanged;
+        public delegate void OnColorChangedHandler(object sender, string color);
+    }
+}
