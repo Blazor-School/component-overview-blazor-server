4146681c7b6509fd05720dbe3950bef8ad86df94
Red Wei <hevilhuy2@gmail.com>
2021-07-16 14:14:53 +0700

    Added the Cascading Parameter example.

---
+++ BlazorComponent/Data/ComponentInteraction/Lamp.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorComponent.Data.ComponentInteraction
+{
+    public class Lamp
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
--- BlazorComponent/Startup.cs
+++ BlazorComponent/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
