037d6c1a8a0fdde6e85cb51d08285c563853d2aa
Red Wei <hevilhuy2@gmail.com>
2021-07-17 18:02:39 +0700

    Added Parameter example and delegate example.

---
+++ BlazorComponent/Data/ComponentInteraction/Hardware.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BlazorComponent.Data.ComponentInteraction
+{
+    public class Hardware
+    {
+        public string Name { get; set; }
+        public DateTime ProduceDate { get; set; }
+    }
+}
---
+++ BlazorComponent/Data/ComponentInteraction/Phone.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorComponent.Data.ComponentInteraction
+{
+    public class Phone
+    {
+        public string Name { get; set; }
+        public double BatteryPercentage { get; set; }
+    }
+}
